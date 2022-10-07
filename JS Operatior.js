5667976396f4a7bdf8f8c8d2659bb174b2e63838
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-07 15:34:30 +0300

    Add files via upload

---
+++ JS Operatior.js
@@ -0,0 +1,24 @@
+console.log(2+2);
+console.log(1+2+3+4+5);
+console.log(20-18);
+console.log(2*3);
+console.log(8/2);
+console.log(3>2);
+console.log(2>3);
+console.log(10==10);
+console.log(10 == 9);
+console.log(4*5)
+
+/* Aritmetic operatiors in JS:
++ addition
+- substraction
+* multiplication
+/ division
+*/
+
+/* Comparison operatiors in JS:
+    >  greater than
+    <  less than
+    == equal
+    */
+   
