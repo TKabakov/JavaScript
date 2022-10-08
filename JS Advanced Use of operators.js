76c673db1eabbe54f4b3ecd2c76eebde192d6321
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-08 08:15:21 +0300

    Update JS Advanced Use of operators.js

--- JS Advanced Use of operators.js
+++ JS Advanced Use of operators.js
@@ -107,3 +107,19 @@ var now = "Now in ";
 var three = 3;
 var d = "D!";
 console.log(now+three+d);
+
+/*
+Task 6: Use the += operator to accumulate values in a variable
+Code a new variable and name it counter, assigning it to the value of 0.
+
+On the next line, use the += operator to increase the value of counter by 5.
+
+On the next line, use the += operator to increase the value of counter by 3.
+
+On the fourth line, console log the value of the counter variable.
+*/
+console.log("Task 6: Use the += operator to accumulate values in a variable");
+var counter = 0;
+counter +=5;
+counter +=3;
+console.log(counter);
