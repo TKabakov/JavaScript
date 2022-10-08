76d4023673e4fd9b7a260f008c749285deb763ac
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-08 08:14:29 +0300

    Update JS Advanced Use of operators.js

--- JS Advanced Use of operators.js
+++ JS Advanced Use of operators.js
@@ -89,3 +89,21 @@ Console log the result of adding two numbers, 5 and 10, using the + operator.
 */
 console.log("Task 4: Add numbers using the + operator");
 console.log(5+10);
+/*
+Task 5: Concatenate numbers and strings using the + operator
+
+Code three variables:
+
+The first variable should be a string with the following value: "Now in ". Name the variable now.
+
+The second variable should be a number with the value: 3. Name the variable three.
+
+The third variable should a string with the following value: "D!". Name the variable d.
+
+Console log the following code: now + three + d.
+*/
+console.log("Task 5: Concatenate numbers and strings using the + operator");
+var now = "Now in ";
+var three = 3;
+var d = "D!";
+console.log(now+three+d);
