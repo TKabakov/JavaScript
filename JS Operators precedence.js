762735c270a417bf70cdc085f74ecd973b84df0d
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-09 19:30:46 +0300

    Update JS Operators precedence.js

--- JS Operators precedence.js
+++ JS Operators precedence.js
@@ -4,15 +4,14 @@ console.log(4*3**2);//4*9
 
 let a;
 let b;
-console.log(a=b=5);
+console.log(a=b=5);//from right to the left
+//it's the same as these 2 steps:
+console.log(b=5);
+console.log(a=b);
 
 console.log(3+10*2);
 console.log(3+(10*2));
 console.log((3+10)*2);
-
-console.log(a=b=5);
-console.log(b=5);
-console.log(a=b);
 
 console.log(4**3**2);//same as 4**(3**2)
 console.log(4/3/2);//smae as (4/3)/2
@@ -26,6 +25,6 @@ function A(){ console.log("called A"); return false};
 function B(){ console.log("called B"); return false};
 function C(){ console.log("called C"); return true};
 
-console.log(C() || B() && A());
+console.log(C() || B() && A());//B() && A() is "shortcut"
 
 console.log(A()&& C() || B())
