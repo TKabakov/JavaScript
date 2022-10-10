6ed48a72c81d54fc8307e0e67b9970a61ed7e90a
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-10 06:13:06 +0300

    Update JS Assigments.js

--- JS Assigments.js
+++ JS Assigments.js
@@ -1,12 +1,14 @@
 //Additional Assignment
 let a = 10;
 a+=5;
+console.log(a);
 console.log(a+=5);
 console.log(a+=5);
 
 //Substraction Assignment
 let b = 10;
 b-=2;
+console.log(b);
 console.log(b-=2);
 console.log(b-=3);
 
@@ -17,7 +19,6 @@ console.log(c*=2);
 console.log(c*3);
 
 //Division Assignment
-
 let d = 10;
 console.log(d/=2);
 console.log(d/=2);
@@ -26,12 +27,12 @@ let f = 10;
 console.log(f%=4);
 console.log(f%=2);
 
+//The shift assignment
 let j = 100;
 console.log(j<<5);
 console.log(j<<8);
 console.log(j<<2);
 
-//The shift assignment
 console.log(j<<=2);
 console.log(j<<=5);
 console.log(j>>=5);
