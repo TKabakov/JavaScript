2c15fe310e33217a74b9c39d515acc35ab4676b9
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-10 06:07:16 +0300

    Add files via upload

---
+++ JS Assigments.js
@@ -0,0 +1,39 @@
+//Additional Assignment
+let a = 10;
+a+=5;
+console.log(a+=5);
+console.log(a+=5);
+
+//Substraction Assignment
+let b = 10;
+b-=2;
+console.log(b-=2);
+console.log(b-=3);
+
+//Multiplication Assignment
+let c = 10;
+c*=2;
+console.log(c*=2);
+console.log(c*3);
+
+//Division Assignment
+
+let d = 10;
+console.log(d/=2);
+console.log(d/=2);
+
+let f = 10;
+console.log(f%=4);
+console.log(f%=2);
+
+let j = 100;
+console.log(j<<5);
+console.log(j<<8);
+console.log(j<<2);
+
+//The shift assignment
+console.log(j<<=2);
+console.log(j<<=5);
+console.log(j>>=5);
+console.log(j>>=2);
+
