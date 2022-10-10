962f044b4a519080ab5ea679bceee598808be13f
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-10 06:06:26 +0300

    Add files via upload

---
+++ JS Arithmetic.js
@@ -0,0 +1,39 @@
+//The + Operator
+let x = 5;
+let y = 2;
+let z = x + y;
+console.log(z);
+
+//The - Operator
+let m = 5;
+let n = 2;
+let p = m - n;
+console.log(p);
+console.log(m-n);
+
+//The ++ Operator (increment)
+
+let a = 5;
+a++;
+let b = a;
+console.log(b);
+console.log(a++);
+
+//The -- Operator(decrement)
+let c = 5;
+c--;
+let d = c;
+console.log(c);
+console.log(c--);
+
+//The ** Operator (exponentiation) and Math.pow operator
+
+let e = 4;
+console.log(e**2);
+console.log(Math.pow(e,4));
+
+//The Operator Pecedence
+console.log(100+50*3)//100+150
+console.log((100+50)*3);//150*3
+console.log(100+50-3);//150-3
+
