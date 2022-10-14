66b4a11d8377aace8012b52f4204132d00d5f6ea
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-14 05:47:15 +0300

    Add files via upload

---
+++ JS for and while loops.js
@@ -0,0 +1,89 @@
+console.log("Task 1")
+/*
+Write a "for" loop that will perform exactly the same repetitive code as this:*/
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log("Counting comleted!");
+
+//The same can be done by loop
+for (i = 1; i < 6; i++){
+    console.log(i);
+}
+console.log("Counting comleted!");
+
+//The same is:
+let m = 1;
+for( ; m < 6; m++){
+    console.log(m);
+}
+console.log("Counting comleted!");
+
+//The same is:
+let n = 1;
+let text = "";
+while( n < 6) {
+    text += n;
+    n++;
+}
+console.log(text);
+console.log("Counting comleted!");
+
+console.log("Task 2")
+/*
+Write a "for" loop that will perform exactly the same repetitive code as this:
+*/
+console.log(5);
+console.log(4);
+console.log(3);
+console.log(2);
+console.log(1);
+console.log("Countdown finished!");
+
+//The same can be down by loop:
+for (i = 5; i > 0; i--){
+    console.log(i);
+}
+console.log("Countdown finished!");
+
+console.log("Task 3");
+/*
+Write a "while" loop that will perform exactly the same repetitive code as this:
+*/
+while( n < 6) {
+    text += n;
+    n++;
+}
+console.log(text);
+console.log("Counting comleted!");
+
+console.log("Task 4");
+/*
+Write a "while" loop that will perform exactly the same repetitive code as this:
+*/
+let p = 5;
+let text3 = ""
+while ( p > 0 ){
+    text3 += p;
+    p = p - 1;
+}
+console.log(text3);
+console.log("Countdown finished!");
+
+console.log("Task 5");
+/*
+Write a "while" loop that will perform exactly the same repetitive code as this:
+*/
+console.log(2018);
+console.log(2019);
+console.log(2020);
+console.log(2021);
+console.log(2022);
+
+let f = 2018;
+while (f < 2023){
+    console.log(f);
+    f++;
+}
