3d4eeab39ef6cfbd283f00caaae73eb6ecee034a
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-17 18:02:39 +0300

    Add files via upload

---
+++ JS Conditionals and loops - test.js
@@ -0,0 +1,51 @@
+var i = 3;
+if(i == 2 || i == 3) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  }
+
+  switch(i) {
+    case 1:
+      console.log("Hello");
+      break;
+    case 2:
+      console.log("Goodnight");
+      break;
+    case 3:
+      console.log("Goodbye");
+      break;
+  }
+
+  var i = 7;
+  if(i <= 5) {
+    console.log("Hello");
+  } else if(i <= 10) {
+    console.log("Goodnight");
+  } else {
+    console.log("Goodbye");
+  }
+
+  var i = 0;
+  while(i < 3) {
+    console.log("Hello");
+    i++;
+  }
+
+  for (i = 0; i < 2; i++) {
+    console.log("Hello");
+}
+
+if(i == 0 && i == 1) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  }
+
+  if(i < 5) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  }
+
+
