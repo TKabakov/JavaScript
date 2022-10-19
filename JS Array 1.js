ea47688095f767878a451561326649ac639b47da
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-19 21:01:56 +0300

    Update JS Array 1.js

--- JS Array 1.js
+++ JS Array 1.js
@@ -1,14 +1,9 @@
 /*
 JavaScript array elements are accessed using numeric indexes (starting from 0).
 */
-
 const myCars = ["Volvo", "Fiat", "BMW"];
 console.log(myCars[0]);
 console.log(myCars[2]);
-/*
-JavaScript array elements are accessed using numeric indexes (starting from 0)
-*/
-console.log(myCars);
 myCars[0] = "Opel";
 console.log(myCars)
 
@@ -24,14 +19,16 @@ console.log(myCars[1]);
 console.log(myCars[0]);
 
 /*
-Objects use names to access its "members".
+in contrast, Objects use names to access its "members".
 */
 const person ={firstName:"John", secondName:"Smith",age:45};
 console.log(person.secondName);
 console.log(person.age);
 
 /*
-You can have objects in an Array. You can have functions in an Array. You can have arrays in an Array:
+You can have objects in an Array. 
+You can have functions in an Array. 
+You can have arrays in an Array:
 */
 
 function myFunction(a, b){
