d328c951e54a60609608bc3ab48d629722faddcb
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-24 06:35:58 +0300

    Add files via upload

--- JS Object 3.js
+++ JS Object 3.js
@@ -14,3 +14,43 @@ const person = {
   console.log(person.age);
   console.log(person.bio());
   console.log(person.introduceSelf());
+
+  const person1 = {
+    name: ["Bob", "Smith"],
+    age: 45,
+  };
+
+function logProperty(propertyName){
+    console.log(person1[propertyName]);
+  };
+
+  logProperty("name");//[ 'Bob', 'Smith' ]
+  logProperty("age");//45
+
+  /*
+  you can also set (update) the value of object members by declaring the member you want to set 
+  (using dot or bracket notation).
+  */
+  person1.age = 55;
+  person1["name"]["last"] = "Cratchit";
+
+  console.log(person1);
+
+  /*
+  You can also create completely new members.
+  */
+person1["eyes"] = "hazel";
+person1.farewell = function(){
+    console.log("Bye everybody!");
+};
+
+console.log(person1["eyes"]);
+
+person1.farewell();
+
+
+
+
+
+
+
