d955112c2e299c3f39c13cc299f6766957389930
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-25 16:59:13 +0300

    Update Error handling.js

--- Error handling.js
+++ Error handling.js
@@ -27,16 +27,20 @@ There is an Reference Error
 My program does not stop
 */
  
- //A ReferenceError gets thrown when, for example, one tries to use variables that haven't been declared anywhere
+/* 
+A ReferenceError gets thrown when, for example, one tries to use variables that haven't been declared anywhere
  console.log(username);//ReferenceError: username is not defined at Object
- 
-//Any kind of invalid JavaScript code will cause a SyntaxError 
+*/
+
+/*
+ Any kind of invalid JavaScript code will cause a SyntaxError
+*/
  let c "there is no assignment operator here";//SyntaxError: Unexpected string
- /*
- SyntaxError in JavaScript: cannot be caught using the try-catch block.
- */
+/*
+Note: SyntaxError in JavaScripe CANNOT be caught using the try-catch block.
+*/
  
- "hello".pop();//TypeError: "hello".pop is not a function at Object.
+ "hello".pop();//returns TypeError: "hello".pop is not a function at Object.
  /*
  A TypeError is thrown when, for example, trying to run a method on a non-supported data type.
  */
