f34e5fb70d8c666cc9aa85164c0ece92bd262ea2
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-25 16:44:25 +0300

    Add files via upload

---
+++ Error handling.js
@@ -0,0 +1,34 @@
+try{
+ console.log(a+b)
+ }catch(err){
+ console.log(err);
+ console.log("There is an error");
+ console.log("The error is saved in the err block");
+ }
+ 
+ console.log("My program does not stop");
+ 
+try{
+ throw new ReferenceError()
+ }catch(error){
+ console.log(error);
+ console.log("There is an Reference Error");
+ }
+ 
+ console.log("My program does not stop");
+ 
+ //A ReferenceError gets thrown when, for example, one tries to use variables that haven't been declared anywhere
+ console.log(username);//ReferenceError: username is not defined at Object
+ 
+//Any kind of invalid JavaScript code will cause a SyntaxError 
+ let c "there is no assignment operator here";//SyntaxError: Unexpected string
+ /*
+ SyntaxError in JavaScript: cannot be caught using the try-catch block.
+ */
+ 
+ "hello".pop();//TypeError: "hello".pop is not a function at Object.
+ /*
+ A TypeError is thrown when, for example, trying to run a method on a non-supported data type.
+ */
+ 
+ 
