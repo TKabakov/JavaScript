066c1713d32f481cef1d97169184852768534812
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-28 05:10:13 +0300

    Add files via upload

--- JS Object Test.js
+++ JS Object Test.js
@@ -1,3 +1,4 @@
+console.log("test1");
 const cat = {
     name: 'Bertie',
     breed: 'Cymric',
@@ -12,25 +13,57 @@ cat.greeting();
 
 cat.color = "black";
 
-let para1 = document.createElement('p');
-let para2 = document.createElement('p');
-
-para1.textContent = `The cat's name is ${ catName }.`;
-para2.textContent = `The cat's color is ${ cat.color }.`;
-
-section.appendChild(para1);
-section.appendChild(para2);
-
+console.log("test2");
 let bandInfo;
-let myBand = {
+bandInfo = {
 name: "Metallica",
 nationality: "US",
 genre: "heavy metal",
 members: 4,
 formed: 1983,
 split:false,
-albums:album1{name:"Ride the Lighting", released:"1983"};
-album2{name:"Master of Puppets", realesed:1986},}
+albums:[{name:"Ride the Lighting", released:1983},
+{name:"Master of Puppets", realesed:1986}],}
 
-bandInfo = `My favorite band is ${myBand.name}. They are form ${myBand.nationality}. The band has ${myBand.members}. The band is formed in ${myBand.formed} and their first album is${myBand.albums.album1}.
+bandInfo = `My favorite band is ${bandInfo.name}. They are from ${bandInfo.nationality}. The band has ${bandInfo.members} members. The band is formed in ${bandInfo.formed} and their first album is ${bandInfo.albums[0].name}.`;
 console.log(bandInfo);
+
+console.log("test3");
+const cat2 = {
+    name : 'Bertie',
+    breed : 'Cymric',
+    color : 'white',
+    greeting: function() {
+      console.log('Hello, said',this.name, this.breed,'!');
+    }
+  }
+
+cat2.greeting();
+  
+  const cat3 = {
+    name : 'Cathy',
+    breed : 'Munchkin',
+    color : 'black',
+    greeting: function() {
+        console.log('Hello, said',this.name, this.breed,'!');
+    }
+  }
+  
+  cat3.greeting();
+
+  console.log("test4");
+  //You can use Constructor
+  function Cat (name,breed,color) {
+    this.name = name;
+    this.breed = breed;
+    this.color = color;
+   this.greeting = function(){
+      console.log('Hello, said',this.name, `the`, this.breed,'!')};
+  };
+
+  const catCat2 = new Cat ("Bertie", "Cymric","white");
+  catCat2.greeting();
+
+  const catCat3 = new Cat ("Cathy", "Munchkin","black");
+  catCat3.greeting();
+
