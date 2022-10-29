8cf3e301d12d23a1dcfe0ae5b345258dbb82e3ab
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-29 08:16:19 +0300

    Add files via upload

---
+++ JS Variables Script.js
@@ -0,0 +1,22 @@
+var petDog = "Rex";
+var petCat = "Pepper";
+
+console.log (petDog);//Rex
+
+console.log (petCat);//Pepper
+
+console.log ("My pet dog's name is:", petDog);//My pet dog's name is: Rex
+
+console.log ("My pet cat's name is:", petCat);//My pet cat's name is: Pepper
+
+var catSound = "purr";
+var dogSound = "woof";
+
+console.log (petDog, "says", dogSound);//Rex says woof
+console.log (petDog + " says " + dogSound);//It is the same: Rex says woof
+
+console.log (petCat, "says", catSound);//Pepper says purr
+
+var catSound = "meow";//variable catSound gets the value of "meow"
+
+console.log (petCat, "now says", catSound);//Pepper now says meow
