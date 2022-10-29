2f1fcba2b013d00478baccc903f9d010e716727e
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-29 08:00:55 +0300

    Add files via upload

---
+++ Return Value.js
@@ -0,0 +1,16 @@
+function doubleIt(num){
+    return num * 2
+}
+function objectMaker(val) {
+    return {
+        prop:val
+    }
+}
+
+console.log(objectMaker(20));
+
+console.log(doubleIt(10));
+console.log(doubleIt(10).toString());
+
+console.log(objectMaker(doubleIt(80)));
+
