bf5f79014d36ae596a2d7a5deadcf1fda774bd96
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-10-31 17:09:43 +0200

    Update JS ternary.js

--- JS ternary.js
+++ JS ternary.js
@@ -13,7 +13,7 @@ function getFee(isMember) {
   
   const greeting = (person) => {
     const name = person ? person.name : "stranger";
-    return "Howdy, ${name}";
+    return `Howdy, ${name}`;
   }
 
   console.log(greeting({ name:"Alice"}));
