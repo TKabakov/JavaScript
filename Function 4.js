572c2306731900d5b14912be7ba5dc5f9e2c466b
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-11-02 04:51:46 +0200

    Add files via upload

---
+++ Function 4.js
@@ -0,0 +1,20 @@
+
+function map(f,a){
+    const result = new Array(a.length);
+    for (let i = 0; i < a.length; i++){
+        result[i] = f(a[i]);
+    }
+    return result;
+}
+
+const f = function (x){
+    return x * x * x;
+}
+
+const numbers = [0, 1, 2, 5, 10];
+const cube = map (f, numbers);
+console.log(cube);
+//function returns [ 0, 1, 8, 125, 1000 ]
+
+
+  
