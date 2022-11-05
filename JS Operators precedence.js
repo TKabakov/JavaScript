2e4f1c4a5af0327de9513a7f15992e979b973959
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-11-05 19:49:42 +0200

    Update JS Operators precedence.js

--- JS Operators precedence.js
+++ JS Operators precedence.js
@@ -1,18 +1,18 @@
-console.log(3+4*5);//3+20
+console.log(3+4*5);//23
 
-console.log(4*3**2);//4*9
+console.log(4*3**2);//36
 
 let a;
 let b;
-console.log(a=b=5);
+console.log(a=b=5);//5
 
-console.log(3+10*2);
-console.log(3+(10*2));
-console.log((3+10)*2);
+console.log(3+10*2);//23
+console.log(3+(10*2));//23
+console.log((3+10)*2);//26
 
-console.log(a=b=5);
-console.log(b=5);
-console.log(a=b);
+console.log(a=b=5);//5
+console.log(b=5);//5
+console.log(a=b);//5
 
 console.log(4**3**2);//same as 4**(3**2)
 console.log(4/3/2);//smae as (4/3)/2
@@ -26,6 +26,6 @@ function A(){ console.log("called A"); return false};
 function B(){ console.log("called B"); return false};
 function C(){ console.log("called C"); return true};
 
-console.log(C() || B() && A());
+console.log(C() || B() && A());//called C true
 
-console.log(A()&& C() || B())
+console.log(A()&& C() || B());//called A called B false
