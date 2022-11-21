f0b489037d134f4d4417d4e31e99b74e518ed7ca
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-11-21 20:07:48 +0200

    Update and rename JS Object Test.js to Object Test.js

--- Object Test.js
+++ Object Test.js
@@ -10,6 +10,7 @@ const cat = {
 let catName = cat["name"];
 
 cat.greeting();
+//Moew!
 
 cat.color = "black";
 
@@ -27,6 +28,7 @@ albums:[{name:"Ride the Lighting", released:1983},
 
 bandInfo = `My favorite band is ${bandInfo.name}. They are from ${bandInfo.nationality}. The band has ${bandInfo.members} members. The band is formed in ${bandInfo.formed} and their first album is ${bandInfo.albums[0].name}.`;
 console.log(bandInfo);
+//My favorite band is Metallica. They are from US. The band has 4 members. The band is formed in 1983 and their first album is Ride the Lighting.
 
 console.log("test3");
 const cat2 = {
@@ -39,6 +41,7 @@ const cat2 = {
   }
 
 cat2.greeting();
+//Hello, said Bertie Cymric !
   
   const cat3 = {
     name : 'Cathy',
@@ -48,8 +51,9 @@ cat2.greeting();
         console.log('Hello, said',this.name, this.breed,'!');
     }
   }
-  
+   
   cat3.greeting();
+//Hello, said Cathy Munchkin !
 
   console.log("test4");
   //You can use Constructor
@@ -63,7 +67,10 @@ cat2.greeting();
 
   const catCat2 = new Cat ("Bertie", "Cymric","white");
   catCat2.greeting();
+//Hello, said Bertie the Cymric !
 
   const catCat3 = new Cat ("Cathy", "Munchkin","black");
   catCat3.greeting();
+//Hello, said Cathy the Munchkin !
+
 
