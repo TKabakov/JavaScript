7c487443197b571e66412c4c74f5ec6e33f2f846
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-11-28 06:07:06 +0200

    Update Assigments.js

--- Assigments.js
+++ Assigments.js
@@ -1,40 +1,44 @@
 //Additional Assignment
 let a = 10;
 a+=5;
-console.log(a+=5);
-console.log(a);
+console.log(a);//15
+console.log(a+=5);//20
+console.log(a);//25
 
 //Substraction Assignment
 let b = 10;
 b-=2;
-console.log(b-=2);
-console.log(b-=3);
-console.log(b);
+console.log(b-=2);//6
+console.log(b-=3);//3
+console.log(b);//3
 
 //Multiplication Assignment
 let c = 10;
 c*=2;
-console.log(c*=2);
-console.log(c*3);
+console.log(c);//20
+console.log(c*=2);//40
+console.log(c*3);//120
 
 //Division Assignment
 
 let d = 10;
-console.log(d/=2);
-console.log(d/=2);
+console.log(d/=2);//5
+console.log(d/=2);//2.5
 
 let f = 10;
-console.log(f%=4);
-console.log(f%=2);
+console.log(f%=4);//2
+console.log(f%=2);//0
 
+//Shift Assignment
+//The shift assignment operators moves the specified amount of bits 
+//to the left or right and assigns the result to the variable.
 let j = 100;
-console.log(j<<5);
-console.log(j<<8);
-console.log(j<<2);
+console.log(j<<5);//3200
+console.log(j<<8);//25600
+console.log(j<<2);//400
 
-//The shift assignment
-console.log(j<<=2);
-console.log(j<<=5);
-console.log(j>>=5);
-console.log(j>>=2);
+console.log(j<<=2);//400
+console.log(j<<=5);//12800
+console.log(j>>=5);//400
+console.log(j>>=2);//100
 
