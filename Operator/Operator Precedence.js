e310d448e6abb9c78bbf0310cf31d47432bfb480
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-11-29 19:21:19 +0200

    Update Operator Precedence.js

--- Operator/Operator Precedence.js
+++ Operator/Operator Precedence.js
@@ -1,54 +1,56 @@
-console.log("Operator Prededence");
-console.log("Multiplication has precedence over addition.");
-console.log("But paranthesis has precedence over multiplication.");
-console.log((100+20)*3);
+//Operator Prededence
+//Multiplication has precedence over addition
+//But paranthesis has precedence over multiplication.
+console.log((100+20)*3);//360
 
 console.log("The . Operator(Member of)");
 const car = {type:"Toyota",model:"Avensis", color:"silver", millage:155000 };
-console.log(car.type);
-console.log(car.model,car.color, car.millage);
+console.log(car.type);//Toyota
+console.log(car.model,car.color, car.millage);//Avensis silver 155000
 
-console.log(car["type"], car["model"]);
+console.log(car["type"], car["model"]);//Toyota Avensis
 
-console.log("Using new Date() without arguments, creates a date object with the current date and time:");
+//Using new Date() without arguments, creates a date object with the current date and time:
 const date1 = new Date()
-console.log(date1);
-console.log(new Date());
+console.log(date1);//2022-11-29T17:07:22.194Z
+console.log(new Date());//2022-11-29T17:07:22.198Z
 
-console.log("Postfix Increment");
-console.log("If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.");
+//Postfix Increment
+//If used postfix, with operator after operand (for example, x++), 
+//the increment operator increments and returns the value before incrementing.
 var a = 8;
 var b = a++;
-console.log(b);
-console.log(a++);
-console.log(a);
+console.log(b);//8
+console.log(a++);//9
+console.log(a);//10
 
 let m = 3;
 const n = m++;
 console.log(m);//m = 4
 console.log(n)//n = 3;
 
-console.log("If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.");
+//If used prefix, with operator before operand (for example, ++x), 
+//the increment operator increments and returns the value after incrementing.
 var j = 8
 var f = ++j;
 console.log(f);//f = j+1 = j = 9;
 console.log(j);//j = 9;
 
-console.log("Postfix Decrement");
+Postfix Decrement
 var c = 8;
 var d = c--;
-console.log(c);
-console.log(d);
+console.log(c);//7
+console.log(d);//8
 
-console.log("The ! Operator (Logical NOT)");
-console.log(5==8);
-console.log(!(5==8));
+//The ! Operator (Logical NOT)
+console.log(5==8);//false
+console.log(!(5==8));//true
 
 console.log("The Shift Operator <<, >>, >>>");
-console.log(5<<2);
-console.log(-5>>1);
-console.log(5>>1);
-console.log(5>>>1);
+console.log(5<<2);//20
+console.log(-5>>1);//-3
+console.log(5>>1);//2
+console.log(5>>>1);//2
 
 
 
