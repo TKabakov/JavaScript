44f14a2096d8d01dc18e0824cef5d28639d889af
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-12-03 18:56:24 +0200

    Add files via upload

---
+++ FinalProject/EndOfCourseAssignment.js
@@ -0,0 +1,50 @@
+const a = 10;
+const b = 5;
+if(a == 7 || b == 5) {
+    console.log("Green");
+} else {
+    console.log("Blue");
+}
+
+var x = 2;
+x += 5;
+console.log(x);
+
+var x = 23.2;
+
+console.log(typeof(x));
+
+var x = 20;
+
+if(x < 5) {
+    console.log("Apple");
+} else if(x > 10 && x < 20) {
+    console.log("Pear");
+} else {
+    console.log("Orange");
+}
+/*
+var result = 0;
+
+var i = 4;
+while(i > 0) {
+    result += 2;
+    i--;
+}
+
+console.log(result);
+*/
+console.log(result);
+var result = 7;
+
+function addTwo(a,b) {
+    return a
+}
+addTwo(5,10);
+
+console.log(addTwo(5,10));
+
+var cat = {}
+cat["sound"] = "meow"
+var catSound = "purr"
+console.log(cat.sound)
