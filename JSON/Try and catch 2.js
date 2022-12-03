8ef0bb5170d0671acb6c95f75009e0fb52ec6430
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-12-03 18:50:45 +0200

    Add files via upload

---
+++ JSON/Try and catch 2.js
@@ -0,0 +1,34 @@
+const { userInfo } = require("os");
+
+try {
+    console.log("Start of try runs");
+
+    unicycle;
+
+    console.log("End of try runs -- never reached");
+
+} catch(err) {
+
+    console.log("Error has occured: " + err.stack);
+
+} finally { 
+    console.log("This is always run");
+
+}
+
+console.log('...Then the execution continues');
+
+let json = '{"age": 30 }';
+
+try{
+    
+let user = JSON.parse(json);
+if(!user.name){
+    throw new SyntaxError ("Incomplete data: no name");
+}
+
+console.log(user.name);
+
+} catch (e) {
+    console.log("JSON Error: " + e);
+}
