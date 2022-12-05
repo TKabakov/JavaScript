55e978791939c277a22cf6336f74364d268f2520
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-12-05 18:22:09 +0200

    Add files via upload

---
+++ 01. First Steps/ScriptElementInHTML.js
@@ -0,0 +1,7 @@
+function myFunction3(){
+    document.getElementById("demo3").innerHTML  = "Paragraph 3 is chanded"
+}
+
+function myFunction4(){
+    document.getElementById("demo4").innerHTML  = "Paragraph 4 is chanded"
+}
