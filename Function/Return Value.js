635892219cbd5dedbdf1c53ea8998115cc26dbf3
TKabakov <114015277+TKabakov@users.noreply.github.com>
2022-12-08 23:47:40 +0200

    Update and rename Return Value.js to Function/Return Value.js

--- Function/Return Value.js
+++ Function/Return Value.js
@@ -14,5 +14,5 @@ console.log(doubleIt(10));//20
 
 console.log(doubleIt(10).toString());//20
 
-console.log(objectMaker(doubleIt(80)));//{ prop: 160 }(prop gets value 80*2
+console.log(objectMaker(doubleIt(80)));//{ prop: 160 }(prop gets value 80*2)
 
